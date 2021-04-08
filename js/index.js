f3ba2b6686258af593d9768353ca3699344355a9
Steven Shropshire <steven.shropshire@getbraintree.com>
2021-04-08 11:20:09 -0500

    Update index.js to always register onComplete callback
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function log(toLog) {
+  document.querySelector('#log').innerHTML += toLog + '\n';
+}
+
+function isPopupBridge() {
+  return (window.popupBridge && window.popupBridge.open);
+}
+
 function click() {
   var url;
   var resultTerm = document.getElementById('resultTerm');
@@ -5,37 +13,15 @@ function click() {
 
   console.log('click!');
 
-  if (window.popupBridge && window.popupBridge.open) {
+  if (isPopupBridge()) {
     var href = window.location.href;
     url = href.substring(0, href.lastIndexOf("/")) + '/this_launches_in_popup.html' +
       '?popupBridgeReturnUrlPrefix=' + window.popupBridge.getReturnUrlPrefix();
 
     window.popupBridge.open(url);
 
-    window.popupBridge.onCancel = function () {
-      resultTerm.innerHTML = 'You did not choose a color';
-      resultData.innerHTML = '';
-    };
-
-    window.popupBridge.onComplete = function (err, payload) {
-      if (err) {
-        resultTerm.innerHTML = 'Error:'
-        resultData.innerHTML = JSON.stringify(err);
-      } else if (payload) {
-        if (payload.path === '/cancel') {
-          resultTerm.innerHTML = 'You did not like any of our colors';
-          resultData.innerHTML = '';
-        } else if (payload.queryItems) {
-          resultTerm.innerHTML = 'Your favorite color:';
-          resultData.innerHTML = payload.queryItems.color;
-        }
-      } else {
-        resultTerm.innerHTML = 'You did not choose a color';
-        resultData.innerHTML = '';
-      }
-    };
   } else {
-    var popup = window.open("this_launches_in_popup.html","DescriptiveWindowName","resizable,scrollbars,status,height=400px,width=400px");
+    var popup = window.open("this_launches_in_popup.html", "DescriptiveWindowName", "resizable,scrollbars,status,height=400px,width=400px");
 
     window.addEventListener('message', function (event) {
       var color = JSON.parse(event.data).color;
@@ -55,7 +41,30 @@ function click() {
 var button = document.getElementsByTagName('button')[0];
 button.addEventListener('click', click);
 
+if (isPopupBridge()) {
+  var resultTerm = document.getElementById('resultTerm');
+  var resultData = document.getElementById('resultData');
 
-function log(toLog) {
-  document.querySelector('#log').innerHTML += toLog + '\n';
+  window.popupBridge.onCancel = function () {
+    resultTerm.innerHTML = 'You did not choose a color';
+    resultData.innerHTML = '';
+  };
+
+  window.popupBridge.onComplete = function (err, payload) {
+    if (err) {
+      resultTerm.innerHTML = 'Error:'
+      resultData.innerHTML = JSON.stringify(err);
+    } else if (payload) {
+      if (payload.path === '/cancel') {
+        resultTerm.innerHTML = 'You did not like any of our colors';
+        resultData.innerHTML = '';
+      } else if (payload.queryItems) {
+        resultTerm.innerHTML = 'Your favorite color:';
+        resultData.innerHTML = payload.queryItems.color;
+      }
+    } else {
+      resultTerm.innerHTML = 'You did not choose a color';
+      resultData.innerHTML = '';
+    }
+  };
 }
