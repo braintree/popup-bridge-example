57a8cc5cd9edf4bff14f25621b1a65c1de729f73
Timothy Chow <tichow@paypal.com>
2024-05-08 15:12:21 -0500

    Fix logic for isAppLink query param

--- js/this_launches_in_popup.js
+++ js/this_launches_in_popup.js
@@ -2,16 +2,13 @@ $('a').on('click', function (event) {
    var color = $(this).data('color');
    var anchor = event.currentTarget.hash.substring(1);
 
-   if (location.search.indexOf('popupBridgeReturnUrlPrefix') !== -1) {
+   if (location.search.lastIndexOf('isAppLink') !== -1 && location.search.split('isAppLink=')[1] == "true") {
+     prefix = "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com"
+     location.href = prefix + '?color=' + color;
+   } else if (location.search.indexOf('popupBridgeReturnUrlPrefix') !== -1) {
      var prefix = location.search.split('popupBridgeReturnUrlPrefix=')[1];
      if (anchor === 'cancel') {
        prefix += 'cancel';
-     }
-
-     if (location.search.lastIndexOf('isAppLink') !== -1) {
-       if (location.search.split('popupBridgeReturnUrlPrefix=')[1] == "true") {
-         prefix = "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com"
-       }
      }
 
      location.href = prefix + '?color=' + color;
